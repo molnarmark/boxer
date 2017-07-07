557d2d8be31cfec0a0f099da3a0600761cf2078b
Mark Molnar <molnarpw@gmail.com>
2017-07-07 18:36:29 +0200

    initial commit

---
+++ boxer.nim
@@ -0,0 +1,27 @@
+import json, strutils, colorize, sequtils
+
+let boxes = readFile("boxes.json").parseJson()
+
+proc generatePadding(inside, side: string, padding: int): string =
+  result = ("\n" & side & (" ".repeat(inside.len - 6)) & side).repeat(padding)
+
+proc Boxer*(text: string, boxType: string, padding: int = 0): string =
+  let textLength = text.len
+  let topLeft = boxes[boxType]["topLeft"].getStr()
+  let topRight = boxes[boxType]["topRight"].getStr()
+  let bottomLeft = boxes[boxType]["bottomLeft"].getStr()
+  let bottomRight = boxes[boxType]["bottomRight"].getStr()
+  let top = boxes[boxType]["horizontal"].getStr()
+  let side = boxes[boxType]["vertical"].getStr()
+
+  var border_top = topLeft & repeat(top, textLength + padding * 2) & topRight
+  var inside = side & " ".repeat(padding) & text & " ".repeat(padding) & side
+  var border_bottom = bottomLeft & repeat(top, textLength + padding * 2) & bottomRight
+
+  # adding padding to the top
+  border_top = border_top & generatePadding(inside, side, padding)
+
+  # adding padding to the bottom
+  inside = inside & generatePadding(inside, side, padding)
+
+  result = (border_top & "\n" & inside & "\n" & border_bottom)
---
+++ boxer_example.nim
@@ -0,0 +1,4 @@
+import boxer, colorize
+
+let b = Boxer("Enigma ezt is megoldotta", "round", 1)
+echo b.bold.fgBlue
